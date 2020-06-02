c7afd9bdad378b383a26e9231624e1653d8315ef
Aayush Agrawal <aayushagrawal135@gmail.com>
2020-06-03 01:09:16 +0530

    Add fallback for title and loops for posts in template

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {"username": "Aayush"}
-    return render_template("index.html", title = "Home", user = user)
+    posts = [
+        {
+            "author": {"username": "Aman"},
+            "body": "HP, Anime, Marvel, DC, Pokemon forever"
+        },
+        {
+            "author": {"username": "Skeny"},
+            "body": "HP forever"
+        }
+    ]
+    return render_template("index.html", title = "Home", user = user, posts = posts)
